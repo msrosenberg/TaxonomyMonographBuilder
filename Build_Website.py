74257beea17458900f01278d64c049c64a780652
Michael Rosenberg <drmsrosenberg@gmail.com>
2019-04-04 13:48:17 -0400

    bugfix - internal page links missing #

--- Build_Website.py
+++ Build_Website.py
@@ -4137,7 +4137,7 @@ def write_systematics_overview(outfile: TextIO, do_print: bool, taxon_ranks: lis
                 outfile.write("      <h3 id=\"" + taxon_link(taxon) + "\">" + start_tag + taxon.name + end_tag + " " +
                               format_reference_cite(refdict[taxon.author], do_print, AUTHOR_NOPCOMMA) + "</h2>\n")
                 if ">" in taxon.type_species:
-                    typestr = "<a href=\"genus_" + taxon.type_species[1:] + "\">Genus <em>" + taxon.type_species[1:] + \
+                    typestr = "<a href=\"#genus_" + taxon.type_species[1:] + "\">Genus <em>" + taxon.type_species[1:] + \
                               "</em></a>"
                 else:
                     s = find_species_by_name(taxon.type_species)
